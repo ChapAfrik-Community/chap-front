c059f4f3fbeffe44791a0253f2d1fd06e9ee4384
graceDev1 <grace.devolop@gmail.com>
2021-02-06 12:45:32 +0200

    initial header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      ChapAfric Community Website
+      {/* navbar */}
+      {/* banner */}
+      <Banner/>
     </div>
   );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../css/Banner.css';
+import background from '../assets/img/christina.png';
+
+export class Banner extends Component {
+    render() {
+        return (
+            <header
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url(${background})`
+            }}
+             className="banner">
+                 <div className="banner__info">
+                    <div>
+                        <h1>ChapAfrik Design </h1>
+                        <h1>Internship 2021</h1>
+                        <p>For Aspiring Young People</p>
+                    </div>
+                    <div>
+                        A 3-month remote internship program targeting <br/> 
+                        entry level designers in the field of UI/UX, 
+                        Graphics and Product design.
+                    </div>
+                    <div>
+                        <button>Apply Now</button>
+                    </div>
+                 </div>
+               
+            </header>
+        )
+    }
+}
+
+export default Banner
